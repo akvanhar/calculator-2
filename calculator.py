8362f74c5845b0ae043fc2eb34337b9a6b66f974
Hackbright Student <info@hackbrightacademy.com>
2015-07-01 17:05:36 -0700

    get user input and add

--- calculator.py
+++ calculator.py
@@ -9,4 +9,10 @@ from arithmetic import *
 
 
 # Your code goes here
-print test
+user_calc_input = raw_input("> ")
+calc_list = user_calc_input.split(" ")
+if calc_list[0] == "+":
+    sum = add(int(calc_list[1]), int(calc_list[2]))
+    print sum
+else:
+    print "I don't understand."
