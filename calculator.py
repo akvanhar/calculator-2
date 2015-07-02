b09c9fdf56ef6044f60a3457ad9574e56dbf2921
Hackbright Student <info@hackbrightacademy.com>
2015-07-02 12:58:37 -0700

    takes negative and floating point exponents for pow

--- calculator.py
+++ calculator.py
@@ -7,9 +7,7 @@ calculator program yourself in this file.
 
 from arithmetic import *
 
-
 # Your code goes here
-calc_input = raw_input("> ").split(" ")
 
 def calculator(calc_list):
     if calc_list[0] == "+":
@@ -31,7 +29,7 @@ def calculator(calc_list):
         cubenum = cube(int(calc_list[1]))
         print cubenum
     elif calc_list[0] == "pow":
-        pownum = power(int(calc_list[1]), int(calc_list[2]))
+        pownum = power(float(calc_list[1]), float(calc_list[2]))
         print pownum
     elif calc_list[0] == "mod":
         modulation = mod(int(calc_list[1]), int(calc_list[2]))
@@ -41,8 +39,8 @@ def calculator(calc_list):
     else:
         print "I don't understand."
 
+calc_input = raw_input("> ").split(" ")
 
-
-while calc_input != ["q"]:
+while calc_input[0] != "q":
     calculator(calc_input)
     calc_input = raw_input("> ").split(" ")
