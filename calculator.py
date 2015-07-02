575113d21ea759539b423d852c6a8c32bae3bfb4
Hackbright Student <info@hackbrightacademy.com>
2015-07-01 17:44:08 -0700

    added a function to simplify code

--- calculator.py
+++ calculator.py
@@ -9,10 +9,40 @@ from arithmetic import *
 
 
 # Your code goes here
-user_calc_input = raw_input("> ")
-calc_list = user_calc_input.split(" ")
-if calc_list[0] == "+":
-    sum = add(int(calc_list[1]), int(calc_list[2]))
-    print sum
-else:
-    print "I don't understand."
+calc_input = raw_input("> ").split(" ")
+
+def calculator(calc_list):
+    if calc_list[0] == "+":
+        addition = add(int(calc_list[1]), int(calc_list[2]))
+        print addition
+    elif calc_list[0] == "-":
+        subtraction = subtract(int(calc_list[1]), int(calc_list[2]))
+        print subtraction
+    elif calc_list[0] == "*":
+        multiplication = multiply(int(calc_list[1]), int(calc_list[2]))
+        print multiplication
+    elif calc_list[0] == "/":
+        division = divide(int(calc_list[1]), int(calc_list[2]))
+        print division
+    elif calc_list[0] == "square":
+        squarednum = square(int(calc_list[1]), int(calc_list[2]))
+        print squarednum
+    elif calc_list[0] == "cube":
+        cubenum = cube(int(calc_list[1]), int(calc_list[2]))
+        print cubenum
+    elif calc_list[0] == "pow":
+        pownum = power(int(calc_list[1]), int(calc_list[2]))
+        print pownum
+    elif calc_list[0] == "mod":
+        modulation = mod(int(calc_list[1]), int(calc_list[2]))
+        print modulation
+    elif calc_list[0] == "q":
+        return
+    else:
+        print "I don't understand."
+
+
+
+while calc_input != ["q"]:
+    calculator(calc_input)
+    calc_input = raw_input("> ").split(" ")
